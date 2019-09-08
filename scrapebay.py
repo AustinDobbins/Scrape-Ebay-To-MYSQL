745628b63c993666617560dd589b887fad33aadc
AustinDobbins <dobbinsaustin11@gmail.com>
2019-09-07 23:33:17 -0700

    Fills all three tables in the MYSQL database with clean data

--- scrapebay.py
+++ scrapebay.py
@@ -1,62 +1,117 @@
-# Scrapes ebay for product search page prices of 200 products and puts them into a list 
+#scrapes ebay sold page 1 at a time and inserts data for each product sold into a three table mysql database
 import requests
 import mysql.connector
 import datetime
 import locale
 from bs4 import BeautifulSoup
 
-URL = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw=oneplus+6t&_sacat=0&_udlo=150&_udhi=450%22&_ipg=200&rt=nc&LH_Sold=1&LH_Complete=1'
+#print statements to request necessary details to perform scrape
+cleanURL = input("What is the Ebay URL? ")
+p_name = input("What is the product name? ")
+product_status = input("Is this product in your database yet? (Please enter yes or no)").lower()
+condition = input("Is the product used or new? ").lower()
+
+#variables for use in mysql queries
+URL = cleanURL 
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0'}
 page = requests.get(URL, headers=headers)
 soup = BeautifulSoup(page.content, 'html.parser')
-results = []
-clean = []
-date = datetime.date.today()
-
-
-
+dateResults =[]
+names = []
+cleanName = []
+priceResults = []
+period_deleted = []
+clean_price = []
+shipping = []
+today = datetime.date.today()
+tupleList = []
 
 #finds all prices on the page and strips html tags
 for price in soup.findAll("span", {"class": "POSITIVE"}):
-    results.append(price.get_text(strip=True))
+    priceResults.append(price.get_text(strip=True))
+
+#finds all dates sold
+for date in soup.findAll("div", {"class": "s-item__title--tag"}):
+    dateResults.append(date.get_text(strip=True))
+
+#finds all product titles
+for title in soup.findAll("h3", {"class": "s-item__title s-item__title--has-tags"}):
+    names.append(title.get_text(strip=True))
 
 #remove false data 
-for i in results:
-	if 'to' in results:
-		results.remove('to')
-for i in results:
-	if '$0.00' in results:
-		results.remove('$0.00')
+for i in priceResults:
+    if 'to' in priceResults:
+        priceResults.remove('to')
+for i in priceResults:
+    if '$0.00' in priceResults:
+        priceResults.remove('$0.00')
 
-#removes dollar signs and converts data to floats, appends clean data clean list
-for i in results:
-  clean.append(float(i[1:]))
+#cleans name list 
+names.pop(0)
 
-sum = sum(map(float,clean))
-len = len(clean)
-avg = sum / len
+#cleans dateResults
+dateResults = dateResults[1:]
 
-locale.setlocale( locale.LC_ALL, 'English_United States.1252' )
-avgPrice = locale.currency( avg, grouping = True )
-
+#remove false data 
+for i in priceResults:
+    if 'to' in priceResults:
+        priceResults.remove('to')
+for i in priceResults:
+    if '$0.00' in priceResults:
+        priceResults.remove('$0.00')
+#preps for converting string to float
+for dollar_sign in priceResults:
+    clean_price.append(dollar_sign[1:])
 
 #mysql database
 mydb = mysql.connector.connect(
     host = 'localhost',
     user = 'root',
     passwd = '',
-    database = 'testdb'
+    database = 'ebay_scraper'
 )
-
 mycursor = mydb.cursor()
 
-sqlFormula = "INSERT INTO average (product_name, scrape_date, avg_sold_price) VALUES (%s, %s, %s)"
-testScrape = ('Oneplus 6t', date, avgPrice)
+#insert into product table
+if 'no' in product_status:
+    query = "INSERT INTO products (product_name) VALUES (%s)"
+    mycursor.execute(query,(p_name,))
+    mydb.commit()
+    print('New product saved')
+    print ('-')
 
-mycursor.execute(sqlFormula, testScrape)
+#pull primary_id from product table using product_name as unique key 
+query_template = """SELECT product_id FROM ebay_scraper.products WHERE product_name = '<REPLACE>'""" 
+pn = p_name
+query = (query_template.replace('<REPLACE>', pn))
+mycursor.execute(query)
+primary_key = mycursor.fetchone()
+clean_key = (primary_key[0])
 
+#insert into scrape_data table 
+scrape_query = "INSERT INTO scrape_data (scrape_date, product_name, product_id) VALUES (%s, %s, %s)"
+mycursor.execute(scrape_query, (today, p_name, clean_key,))
 mydb.commit()
+print('scrape_data entry saved')
+print ('-')
 
+#pull scrape_id from database for use in product_data table
+scrape_pull = """SELECT scrape_id FROM scrape_data WHERE product_id = '<REPLACE>' ORDER BY scrape_id DESC LIMIT 1""" 
+scrape_pull_query = (scrape_pull.replace('<REPLACE>', str(clean_key)))
+mycursor.execute(scrape_pull_query)
+scrape_primary_key = mycursor.fetchall()
+scrape_key = (scrape_primary_key[0])
+clean_scrape = scrape_key[0]
 
+#creates tuple list for use in product_data querie, 1 tuple = 1 row 
+for a, b, c in zip(names, dateResults, clean_price):
+    tupleList.append(tuple([a, clean_key, condition, b, c, str(clean_scrape)],))
 
-
+#inserts data into product_data table, 1 querie for each row 
+for i in tupleList:
+    mycursor = mydb.cursor()
+    sqlFormula = "INSERT INTO product_data (product_title, product_id, cond, date_sold, price_sold, scrape_id) VALUES (%s, %s, %s, %s, %s, %s)"
+    mycursor.execute(sqlFormula, i)
+    mydb.commit()
+    print('product_data entry saved')
+    print ('-') 
