7858327a29254d05f3d1eda882eb34252dae3115
AustinDobbins <dobbinsaustin11@gmail.com>
2019-10-27 22:18:05 -0700

    Added SQL query to delete old data before scraping the same product

--- scrapebay.py
+++ scrapebay.py
@@ -3,14 +3,14 @@ import requests
 import mysql.connector
 import datetime
 import locale
+import re
 from bs4 import BeautifulSoup
 
 #print statements to request necessary details to perform scrape
 cleanURL = input("What is the Ebay URL? ")
-p_name = input("What is the product name? ")
-product_status = input("Is this product in your database yet? (Please enter yes or no)").lower()
+pn = input("What is the product name? ")
 condition = input("Is the product used or new? ").lower()
-pages_available = input('How many pages of data does ebay have? ')
+pages_available = input('How many pages of data do you want? ')
 
 def scrape_page():
     #variables for storing/cleaning data for mysql queries
@@ -34,7 +34,7 @@ def scrape_page():
         priceResults.append(price.get_text(strip=True))
     
     #finds all dates sold
-    for date in soup.findAll("div", {"class": "s-item__title--tag"}):
+    for date in soup.findAll("span", {"s-item__ended-date s-item__endedDate"}):
         dateResults.append(date.get_text(strip=True))
 
     #finds all product titles
@@ -49,24 +49,11 @@ def scrape_page():
         if '$0.00' in priceResults:
             priceResults.remove('$0.00')
 
-    #cleans name list 
-    names.pop(0)
-
-    #cleans dateResults
-    dateResults = dateResults[1:]
-
-    #remove false data 
-    for i in priceResults:
-        if 'to' in priceResults:
-            priceResults.remove('to')
-    for i in priceResults:
-        if '$0.00' in priceResults:
-            priceResults.remove('$0.00')
-    #preps for converting string to float
+    #preps price for converting string to float
     for dollar_sign in priceResults:
         clean_price.append(dollar_sign[1:])
 
-    #mysql database
+    #mysql database connection
     mydb = mysql.connector.connect(
         host = 'localhost',
         user = 'root',
@@ -76,16 +63,16 @@ def scrape_page():
     mycursor = mydb.cursor()
 
     #insert into product table
-    if 'no' in product_status and URL == cleanURL + '1':
-        query = "INSERT INTO products (product_name) VALUES (%s)"
-        mycursor.execute(query,(p_name,))
-        mydb.commit()
-        print('New product saved')
-        print ('-')
+    query = 'DELETE FROM products WHERE product_name = ' + "'" + pn + "'"
+    mycursor.execute(query)
+    query = "INSERT INTO products (product_name) VALUES (%s)"
+    mycursor.execute(query,(pn,))
+    mydb.commit()
+    print('New product saved')
+    print ('-')
 
     #pull primary_id from product table using product_name as unique key 
     query_template = """SELECT product_id FROM ebay_scraper.products WHERE product_name = '<REPLACE>'""" 
-    pn = p_name
     query = (query_template.replace('<REPLACE>', pn))
     mycursor.execute(query)
     primary_key = mycursor.fetchone()
@@ -93,7 +80,7 @@ def scrape_page():
 
     #insert into scrape_data table 
     scrape_query = "INSERT INTO scrape_data (scrape_date, product_name, product_id) VALUES (%s, %s, %s)"
-    mycursor.execute(scrape_query, (today, p_name, clean_key,))
+    mycursor.execute(scrape_query, (today, pn, clean_key,))
     mydb.commit()
     print('scrape_data entry saved')
     print ('-')
@@ -106,20 +93,23 @@ def scrape_page():
     scrape_key = (scrape_primary_key[0])
     clean_scrape = scrape_key[0]
 
-    #creates tuple list for use in product_data querie, 1 tuple = 1 row 
-    for a, b, c in zip(names, dateResults, clean_price):
-        tupleList.append(tuple([a, clean_key, condition, b, c, str(clean_scrape)],))
+    #creates tuple list for use in product_data querie, 1 tuple = 1 row in product_data table 
+    for name, date, price in zip(names, dateResults, clean_price):
+        tupleList.append(tuple([name, clean_key, condition, date, price, str(clean_scrape)],))
 
     #inserts data into product_data table, 1 querie for each row 
-    for i in tupleList:
+    for product_data in tupleList:
         mycursor = mydb.cursor()
+        delete_old_data = 'DELETE FROM product_data WHERE product_id =' + str(clean_key)
+        mycursor.execute(delete_old_data)
         sqlFormula = "INSERT INTO product_data (product_title, product_id, cond, date_sold, price_sold, scrape_id) VALUES (%s, %s, %s, %s, %s, %s)"
-        mycursor.execute(sqlFormula, i)
+        mycursor.execute(sqlFormula, product_data)
         mydb.commit()
-        #save_number = save_number + 1
-        print('product_data entry saved')
+        print('New product_data entry saved')
         print ('-')
 
+        
+        
 #calls the script to be ran once for every page that ebay has data for (as specified by user through input statement)         
 for pages_unscraped in range(int(pages_available)+1):
     if (pages_unscraped > 0):
